d7dff12ebea730dfab20d272bff17631c71a2db0
isfopo <55164207+isfopo@users.noreply.github.com>
2020-11-06 12:11:59 -0500

    creates input field for total number of questions

--- src/components/TotalQuestionInput.js
+++ src/components/TotalQuestionInput.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
 export const TotalQuestionInput = props => {
+
     return (
-        <div>
-            
+        <div className="totalQuestionInput">
+            <label htmlFor="Number of Questions">Number of Questions</label>
+            <input 
+                onChange={ e => props.handleSetTotalQuestions(parseInt(e.target.value))}
+                type="number" 
+                id="Number of Questions" 
+                name="Number of Questions"
+                defaultValue="10"
+                min="1" max="50"
+            />
         </div>
     )
 }
