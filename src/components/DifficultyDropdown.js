26333ac1a648f9880c680bbc4bd227308c82744e
isfopo <55164207+isfopo@users.noreply.github.com>
2020-11-06 13:04:56 -0500

    adds difficulty dropdown

--- src/components/DifficultyDropdown.js
+++ src/components/DifficultyDropdown.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
-export const DifficultyDropdown = () => {
+export const DifficultyDropdown = props => {
+    
     return (
-        <div>
+        <div className="difficultyMenu">
+            <label htmlFor="difficulty">Difficulty</label>
+            <select name="Difficulty" id="difficulty" onChange={ e => props.handleSetDifficulty(e.target.value)}>
             
+                <option value="0">Any</option>
+                <option value="easy">Easy</option>
+                <option value="medium">Medium</option>
+                <option value="hard">Hard</option>
+
+            </select>        
         </div>
     )
 }
