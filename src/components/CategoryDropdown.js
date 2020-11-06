1b9650d6855d28db25783829ae56b154f0c4669d
isfopo <55164207+isfopo@users.noreply.github.com>
2020-11-06 12:42:54 -0500

    gets and displays category options

--- src/components/CategoryDropdown.js
+++ src/components/CategoryDropdown.js
@@ -1,16 +1,35 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
-export const CategoryDropdown = () => {
+export const CategoryDropdown = props => {
 
-    // "Any" should input an empty string
+    const [categoryOptions, setCategoryOptions] = useState([]);
+
+    const getCategories = () => {
+        fetch(`https://opentdb.com/api_category.php`)
+            .then( response => {
+                return response.json();
+            })
+            .then( response => {
+                setCategoryOptions(response.trivia_categories);
+            })
+    }
 
     useEffect(() => {
-        // getCategories()
+        getCategories()
     }, [])
 
     return (
-        <div>
+        <div className="categoryMenu">
+            <label htmlFor="category">Category</label>
+            <select name="Category" id="category" onChange={ e => props.handleSetCategory(e.target.value)}>
             
+                <option value="0">Any</option>
+                
+                {categoryOptions.map( category => (
+                    <option key={category.id} value={category.id}>{category.name}</option>
+                ))}
+
+            </select>        
         </div>
     )
 }
