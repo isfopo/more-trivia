78a220156e040f70613dc323bf9969d2fcd53844
isfopo <55164207+isfopo@users.noreply.github.com>
2020-11-05 22:50:08 -0500

    changes title in display

--- src/components/HomeDisplay.js
+++ src/components/HomeDisplay.js
@@ -4,7 +4,7 @@ import "../styles/HomeDisplay.css";
 export const HomeDisplay = () => {
     return (
         <div className="homeDisplay">
-            <h1>Tandem Trivia</h1>
+            <h1>More Trivia</h1>
             <h2>Isaac Poole</h2>
         </div>
     )
