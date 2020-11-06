41c056019837b067edf1269ecddacda0f260663d
isfopo <55164207+isfopo@users.noreply.github.com>
2020-11-06 10:48:41 -0500

    decodes special charaters from choices

--- src/components/ChoiceDisplay.js
+++ src/components/ChoiceDisplay.js
@@ -3,6 +3,11 @@ import "../styles/ChoiceDisplay.css";
 
 export const ChoiceDisplay = props => {
 
+    function decode(input) {
+        var doc = new DOMParser().parseFromString(input, "text/html");
+        return doc.documentElement.textContent;
+      }
+
     const orderStyle = [
         {
             order: `${props.order[0]}`
@@ -33,23 +38,23 @@ export const ChoiceDisplay = props => {
             <button 
                 className={`btn ${ !props.isAsking && "correct"}`}
                 onClick={() => submitAnswer(0)}
-                style={orderStyle[0]} >{props.correct}</button>
+                style={orderStyle[0]} >{decode(props.correct)}</button>
 
             <button 
                 className={`btn ${ !props.isAsking && `${ props.selected === 1 ? "incorrect" : "disabled"}`}`}
                 onClick={() => submitAnswer(1)}
-                style={orderStyle[1]} >{props.incorrect[0]}</button>
+                style={orderStyle[1]} >{decode(props.incorrect[0])}</button>
 
             <button 
                 className={`btn ${ !props.isAsking && `${ props.selected === 2 ? "incorrect" : "disabled"}`}`}
                 onClick={() => submitAnswer(2)}
-                style={orderStyle[2]} >{props.incorrect[1]}</button>
+                style={orderStyle[2]} >{decode(props.incorrect[1])}</button>
 
             { props.incorrect.length === 3 &&
                 <button 
                     className={`btn ${ !props.isAsking && `${ props.selected === 3 ? "incorrect" : "disabled"}`}`}
                     onClick={() => submitAnswer(3)}
-                    style={orderStyle[3]} >{props.incorrect[2]}</button>
+                    style={orderStyle[3]} >{decode(props.incorrect[2])}</button>
             }
         </div>
     )
